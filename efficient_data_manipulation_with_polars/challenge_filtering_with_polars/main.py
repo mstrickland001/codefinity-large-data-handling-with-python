698aaa02c2b7009a352230c01da35dc3c627dab4
mstrickland001 <>
2026-06-07 20:00:07 +0000

    Challenge: Filtering with Polars

---
+++ efficient_data_manipulation_with_polars/challenge_filtering_with_polars/main.py
@@ -0,0 +1,14 @@
+import polars as pl
+
+def filter_above_threshold(df, column, threshold):
+    googol = df.filter(pl.col(column) > threshold)
+    return googol
+
+data = {
+    "customer_id": [101, 102, 103, 104, 105],
+    "purchase_amount": [120.5, 99.9, 200.0, 75.0, 305.5]
+}
+df = pl.DataFrame(data)
+
+result = filter_above_threshold(df, "purchase_amount", 100.0)
+print(result)
